13244fe88f0aace6709b95e30a2a177ab4edaa32
ChaosKai <kai-waggeling@web.de>
2016-10-02 09:53:56 +0200

    Update tableau.dev.js

--- tableau.dev.js
+++ tableau.dev.js
@@ -104,7 +104,7 @@ function sendData(data) {
     $.ajax({
         url: 'https://tableau.fbmf.de/input.php',
         method: 'POST',
-        dataType: "json",
+        dataType: "jsonp",
         data: {
             'stationData': data
         },
