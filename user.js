d7e49330366ffd38bbde9abeaf69de25aad3db15
ChaosKai <kai-waggeling@web.de>
2016-10-13 00:09:55 +0200

    Update and rename tableau.user.js to user.js

--- user.js
+++ user.js
@@ -1,6 +1,8 @@
 // ==UserScript==
 // @name        Fahrzeugtableau
 // @namespace   Leitstellenspiel
+// @description Export der Wachen & Fahrzeuge aus dem LSS zum Fahrzeugtableau
+// @downloadURL https://github.com/ChaosKai/fahrzeugtableau/raw/master/user.js
 // @include     http*://www.leitstellenspiel.de/*
 // @version     dev
 // @author      Eagle, ChaosKai93, lost
@@ -9,5 +11,5 @@
 
 var scriptElement = document.createElement("script");
 scriptElement.type = "text/javascript";
-scriptElement.src = "https://rawgit.com/ChaosKai/fahrzeugtableau/master/tableau.dev.js";
+scriptElement.src = "https://rawgit.com/ChaosKai/fahrzeugtableau/master/dev.js";
 document.body.appendChild(scriptElement);
