94df8fe11930d5a1bb18d96062a411db02496f90
ChaosKai <kai-waggeling@web.de>
2017-12-04 00:16:29 +0100

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,1 +1,33 @@
-Test
+
+//  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+//  -
+//  -           Fahrzeugtableau - Autoloader
+//  -
+//  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+//      -
+//      -           UI-Creator
+//      -
+
+        var scriptElement = document.createElement("script");
+        scriptElement.type = "text/javascript";
+        scriptElement.src = "https://rawgit.com/ChaosKai/fahrzeugtableau/master/module-ui.js";
+        document.body.appendChild(scriptElement);
+
+//      -
+//      -           Building- & Vehicle Export
+//      -
+
+        var scriptElement = document.createElement("script");
+        scriptElement.type = "text/javascript";
+        scriptElement.src = "https://rawgit.com/ChaosKai/fahrzeugtableau/master/module-export.js";
+        document.body.appendChild(scriptElement);
+
+//      -
+//      -           Vehicle Updater
+//      -
+
+        var scriptElement = document.createElement("script");
+        scriptElement.type = "text/javascript";
+        scriptElement.src = "https://rawgit.com/ChaosKai/fahrzeugtableau/master/module-update.js";
+        document.body.appendChild(scriptElement);
