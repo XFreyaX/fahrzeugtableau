0ecd37375036af338712d5970ad313fd7736c1a1
ChaosKai <kai-waggeling@web.de>
2017-12-04 02:15:03 +0100

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -12,32 +12,34 @@
         if( typeof killSwitchTableau !== "undefined" )
         {
             console.log("Fahrzeugtableau durch Sebastian gesperrt!");
-            return false;
         }
+        else
+        {
+//          -
+//          -           UI-Creator
+//          -
 
-//      -
-//      -           UI-Creator
-//      -
+            var scriptElement = document.createElement("script");
+            scriptElement.type = "text/javascript";
+            scriptElement.src = "https://rawgit.com/ChaosKai/fahrzeugtableau/master/module/user-interface.js";
+            document.body.appendChild(scriptElement);
 
-        var scriptElement = document.createElement("script");
-        scriptElement.type = "text/javascript";
-        scriptElement.src = "https://rawgit.com/ChaosKai/fahrzeugtableau/master/module/user-interface.js";
-        document.body.appendChild(scriptElement);
+//          -
+//          -           Building- & Vehicle Export
+//          -
 
-//      -
-//      -           Building- & Vehicle Export
-//      -
+            var scriptElement = document.createElement("script");
+            scriptElement.type = "text/javascript";
+            scriptElement.src = "https://rawgit.com/ChaosKai/fahrzeugtableau/master/module/export.js";
+            document.body.appendChild(scriptElement);
 
-        var scriptElement = document.createElement("script");
-        scriptElement.type = "text/javascript";
-        scriptElement.src = "https://rawgit.com/ChaosKai/fahrzeugtableau/master/module/export.js";
-        document.body.appendChild(scriptElement);
+//          -
+//          -           Vehicle Updater
+//          -
 
-//      -
-//      -           Vehicle Updater
-//      -
-
-        var scriptElement = document.createElement("script");
-        scriptElement.type = "text/javascript";
-        scriptElement.src = "https://rawgit.com/ChaosKai/fahrzeugtableau/master/module/update.js";
-        document.body.appendChild(scriptElement);
+            var scriptElement = document.createElement("script");
+            scriptElement.type = "text/javascript";
+            scriptElement.src = "https://rawgit.com/ChaosKai/fahrzeugtableau/master/module/update.js";
+            document.body.appendChild(scriptElement);
+            
+        }
